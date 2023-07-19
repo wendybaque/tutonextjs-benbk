748ef0b59ba28365edbe9cfb767f1e2092c53b53
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-07-19 09:45:47 +0200

    Initialize project

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client"
+import Image from 'next/image';
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  )
+}
