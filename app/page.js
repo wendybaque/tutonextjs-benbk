6f580336df7ff076ef8a76497321670239ed6e1e
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-07-19 10:09:28 +0200

    Add router

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 "use client"
-import Image from 'next/image';
-import styles from "./page.module.css";
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main>
+      <Link href="/">Home</Link>
+      <Link href="/cv">CV</Link>
+      <Link href="/contact">Contact</Link>
     </main>
   )
 }
